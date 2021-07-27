1942ad6848ae0257e789fb54b775cfe79138b415
san7097kumar <sandeep7097kumar@gmail.com>
2021-07-27 20:54:40 +0530

    update config file

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -37,7 +37,7 @@
                 <img src="/images/lary-avatar.svg" alt="Lary avatar">
                 <div class="ml-3">
                     <h5 class="font-bold"><a href="/?author={{$post->author->username}}">{{$post->author->name}}</a></h5>
-                    <h6>Mascot at Laracasts</h6>
+                    <h6>Mascot at Laracasts Git</h6>
                 </div>
             </div>
 
